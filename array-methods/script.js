5393823b052aac6fb9d70585b30cd553a955cd4c
mjarraya <mjarraya@student.42.fr>
2020-01-10 12:27:24 +0100

    filter

--- array-methods/script.js
+++ array-methods/script.js
@@ -21,13 +21,182 @@ const squares = digits.map(function(digit) {
 console.log(digits);
 console.log(squares);
 
-console.clear();
-
 const cities = ["berlin", "amsterdam", "london"];
 
-const upperCasedCities = cities.map(function(city, index) {
-  console.log(city, index);
+const upperCasedCities = cities.map(function(city, index, array) {
+  //   console.log(city, index, array);
   return city.toUpperCase();
 });
 
-console.log(upperCasedCities);
+// console.log(upperCasedCities);
+
+function customMap(array, callback) {
+  const result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const value = callback(array[i], i, array);
+    result.push(value);
+  }
+
+  return result;
+  //   map returns an array, a new array, of the same length of the original array
+}
+
+function customForEach(array, callback) {
+  for (let i = 0; i < array.length; i++) {
+    callback(array[i], i, array);
+  }
+  //   does not return anything
+}
+
+const garage = [
+  {
+    make: "Toyota",
+    model: "Prius",
+    year: 2004,
+    miles: 150000
+  },
+  {
+    make: "BMW",
+    model: "3 series",
+    year: 2010,
+    miles: 90000
+  },
+  {
+    make: "Audi",
+    model: "A4",
+    year: 2010,
+    miles: 80000
+  }
+];
+
+// ["Toyota", "BMW", "Audi"]
+const carMakes = garage.map(function(car) {
+  return car.make;
+});
+
+// Create a `newGarage` array where all the cars would also have a `kms` property that would be converted from the `miles`
+
+// const newGarage = customMap(garage, function(car) {
+const newGarage = garage.map(function(car) {
+  //   car.kms = car.miles * 1.6; ❌ mutates the objects in the original array
+  //   return car; ❌
+
+  return {
+    make: car.make,
+    model: car.model,
+    year: car.year,
+    miles: car.miles,
+    kms: car.miles * 1.6
+  };
+
+  //   return {
+  //     ...car,
+  //     kms: car.miles * 1.6
+  //   };
+
+  //   const carShallowCopy = Object.assign({}, car);
+  //   carShallowCopy.kms = car.miles * 1.6;
+
+  //   return carShallowCopy;
+});
+
+console.table(newGarage);
+
+console.clear();
+
+/*
+let sum = 0;
+for (const number of [1, 3, 5]) {
+  sum += number;
+}
+*/
+
+// digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const sum = [1, 3, 5].reduce(function(accumulator, currentValue) {
+  //   console.log(accumulator, currentValue);
+  return accumulator + currentValue;
+});
+
+const carModels = garage.reduce(function(accumulator, value, index, array) {
+  //
+  if (index === array.length - 1) {
+    // this is the last element
+    return accumulator + value.model;
+  }
+  return accumulator + value.model + ", ";
+}, "");
+
+const upperCasedCarMakesString = carMakes.reduce(function(
+  accumulator,
+  value,
+  index,
+  array
+) {
+  if (index === array.length - 1) {
+    // this is the last element
+    return accumulator + value.toUpperCase();
+  }
+  return accumulator + value.toUpperCase() + ", ";
+});
+
+const sumMileages = garage.reduce(function(accumulator, currentCar) {
+  return accumulator + currentCar.miles;
+}, 0);
+
+// const odds = [];
+
+// digits.forEach(function(digit) {
+//   if (digit % 2 === 1) {
+//     odds.push(digit);
+//   }
+// });
+
+const odds = digits.filter(function(digit) {
+  if (digit % 2 === 1) {
+    return true;
+  }
+});
+
+const students = [
+  {
+    name: "Mia",
+    language: "finnish"
+  },
+  {
+    name: "Elsa",
+    language: "greek"
+  },
+  {
+    name: "Ciwan",
+    language: "arabic"
+  },
+  {
+    name: "Johnny",
+    language: "english"
+  },
+  {
+    name: "Tim",
+    language: "english"
+  }
+];
+
+const englishSpeakers = students
+  .filter(function(student) {
+    if (student.language === "english") {
+      return true;
+    }
+  })
+  .map(function(student) {
+    return student.name;
+  });
+
+// const englishSpeakers = students.reduce(function(accumulator, currentStudent) {
+//   if (currentStudent.language === "english") {
+//     accumulator.push(currentStudent.name);
+//   }
+//   return accumulator;
+// }, []);
+
+// ["Johnny", "Tim"]
